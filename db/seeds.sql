070f470f74f5b00a0cc062d9d5176e7b184479d7
Perkyderm <ncgoedecke@gmail.com>
2021-03-02 00:08:27 -0700

    JAWDB

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO burgers (name) VALUES ('Classic');
 INSERT INTO burgers (name) VALUES ('Nutty Professor');
 INSERT INTO burgers (name) VALUES ('Luther');
-INSERT INTO burgers (name) VALUES ('Flaming Tiki)';
+INSERT INTO burgers (name) VALUES ('Flaming Tiki');
 
